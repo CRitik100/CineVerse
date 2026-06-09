d12b25dcb8dcb4c70ec1075be073682661685a10
rchaur1 <ritik.chaurasiya@morningstar.com>
2026-06-09 06:47:27 +0530

    Renaming the repo name to CineVerse

--- src/component/aiSearch/searchedMovieContainer.jsx
+++ src/component/aiSearch/searchedMovieContainer.jsx
@@ -1,16 +1,18 @@
 import { useSelector } from "react-redux";
 import useSearchMovie from "../../hooks/useSearchMovie";
 import MovieList from "../browse/MovieList";
+import lang from "../../utils/langConstant";
 
 const SearchedMovieContainer = () => {
   const movies = useSelector((store) => store.movies.searchedMovies);
   const moviesData = useSelector((store) => store.movies.searchedMoviesData);
+  const defLang = useSelector((store) => store.appConfig.defaultLanguage);
   console.log("Searched Movies: ", movies);
   useSearchMovie(movies);
 
   return (
     <div className="p-7 overflow-hidden">
-      <MovieList title={"Recommended Movies"} movies={moviesData} />
+      <MovieList title={lang[defLang].recommendedMovies} movies={moviesData} />
     </div>
   );
 };
