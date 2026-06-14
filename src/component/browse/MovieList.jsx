f3d6c616d5359c1450418aa27ee9c766124ce66d
Ritik Chaurasiya <137926949+CRitik100@users.noreply.github.com>
2026-06-14 16:42:08 +0530

    update in favourite model.

--- src/component/browse/MovieList.jsx
+++ src/component/browse/MovieList.jsx
@@ -6,11 +6,11 @@ const MovieList = (props) => {
 
   return (
     <div id="movieList" className="relative hover:z-10">
-      <h1 id="title" className="text-xl mb-3 font-semibold">
+      <h1 id="title" className={`text-xl mb-3 font-semibold ${isFavList ? "md:pl-32" : ""}`}>
         {title}
       </h1>
       <div
-        className={`flex gap-3 overflow-x-scroll no-scrollbar md:py-32 md:-my-32 ${isFavList ? " md:flex-wrap pr-32 -mr-32" : ""}`}
+        className={`flex gap-3 overflow-x-scroll no-scrollbar md:py-32 md:-my-32 ${isFavList ? " md:flex-wrap md:px-32" : ""}`}
       >
         {movies.map((movieData) => (
           <MovieCard key={movieData?.id} movie={movieData} />
